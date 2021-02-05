b0bf2591d3651c0e6823ecb6ed01508c4bfdf68a
Niels <nbijl@petersplanet.nl>
2021-02-05 18:56:17 +0100

    documentatie toegevoegd

--- BucketSort/Analyse.py
+++ BucketSort/Analyse.py
@@ -7,29 +7,46 @@ sys.setrecursionlimit(10 ** 9)
 
 
 def testSortTime(algorithm, array: list):
+    """
+    Deze functie is te gebruiken voor het timen van sorteer algoritmes
+    :param algorithm: Het sorteer algoritme die getimet wil worden
+    :param array: De array waarop het sorteer algoritme getest moet worden
+    :return: De tijd die het duurde voor het algoritme om de meegegeven lijst te sorteren
+    """
     start_time = time.time()
     algorithm(array)
-    return time.time() - start_time
+    return time.time() - start_time  # current time - start time
 
 
 def plotTimeComplexity(algorithm):
+    """
+    Het plotten van de tijdscomplexiteit van het mee gekregen algoritme
+    :param algorithm: Het sorteer algoritme die plot moet worden
+    """
     print("\nHet plotten van de tijd complexiteit.....")
     x = []
     y = []
     for i in range(1000, 76000, 1000):
-        testList = random.sample(range(-1000000, 1000000), i)
+        testList = random.sample(range(-1000000, 1000000), i)  # i aantal getallen tussen de -1000000 en 1000000
         time = testSortTime(algorithm, testList)
         x.append(i)
         y.append(time)
     plt.plot(x, y)
     plt.show()
 
+"""
+De test cases voor de analyse:
+"""
 
 randomTestLists = [random.sample(range(-1000000, 1000000), 1000),
                    random.sample(range(-1000000, 1000000), 10000),
                    random.sample(range(-1000000, 1000000), 30000)]
 sortedTestList = sorted(randomTestLists[2])
 reversedSortedTestList = sorted(randomTestLists[2], reverse=True)
+
+"""
+Het printen van de analyse
+"""
 
 print("Het sorteren van de lijst met random 1.000 items duurde:", testSortTime(bucketSort, randomTestLists[0]), "seconden.")
 print("Het sorteren van de lijst met random 10.000 items duurde:", testSortTime(bucketSort, randomTestLists[1]), "seconden.")
@@ -45,6 +62,11 @@ print("Het recursive sorteren van de lijst met random 30.000 items duurde:", tes
 print("Het recursive sorteren van de lijst met gesorteerde 30.000 items duurde:", testSortTime(bucketSortRecursive, sortedTestList), "seconden.")
 print("Het recursive sorteren van de lijst met omgekeerde gesorteerde 30.000 items duurde:", testSortTime(bucketSortRecursive, reversedSortedTestList), "seconden.")
 
+
+
+"""
+Het plotten van de tijdscompexiteit
+"""
 
 plotTimeComplexity(bucketSortRecursive)
 plotTimeComplexity(bucketSort)
--- BucketSort/BucketSort.py
+++ BucketSort/BucketSort.py
@@ -2,6 +2,14 @@ import sys
 
 
 def bucketSortFunctionalRecursive(data: list, maxDigitPosition: int, digitPosition: int = -1) -> list:
+    """
+    Plaats elke waarde van de een-dimensionale array in een rij van de bucket array, gebaseerd op het meest rechtse cijfer in het getal (de "een"-waarde). Bijvoorbeeld, 97 wordt geplaatst in rij 7, 3 wordt geplaatst in rij 3 en 100 wordt geplaatst in rij 0. Deze stap heet de distribution pass.
+    Loop door de bucket array rij voor rij, en kopieer de waardes terug in de originele array. Deze stap heet de gathering pass. De volgorde van de hierboven genoemde getallen is dus nu 100, 3, 97.
+    Herhaal dit proces voor elke volgende digit-positie (dus voor de tientallen, honderdtallen, etc.). Na de laatste gathering pass is de array gesorteerd. Dit wordt recursive gedaan.
+
+    :param data: De lijst die gesorteerd moet worden
+    :return: De gesorteerde lijst
+    """
     buckets = [[], [], [], [], [], [], [], [], [], []]
     for item in data[:]:  # distribution pass
         if int(len(str(item))) >= abs(digitPosition):
@@ -17,6 +25,14 @@ def bucketSortFunctionalRecursive(data: list, maxDigitPosition: int, digitPositi
 
 
 def bucketSortFunctional(data: list) -> list:
+    """
+    Plaats elke waarde van de een-dimensionale array in een rij van de bucket array, gebaseerd op het meest rechtse cijfer in het getal (de "een"-waarde). Bijvoorbeeld, 97 wordt geplaatst in rij 7, 3 wordt geplaatst in rij 3 en 100 wordt geplaatst in rij 0. Deze stap heet de distribution pass.
+    Loop door de bucket array rij voor rij, en kopieer de waardes terug in de originele array. Deze stap heet de gathering pass. De volgorde van de hierboven genoemde getallen is dus nu 100, 3, 97.
+    Herhaal dit proces voor elke volgende digit-positie (dus voor de tientallen, honderdtallen, etc.). Na de laatste gathering pass is de array gesorteerd.
+
+    :param data: De lijst die gesorteerd moet worden
+    :return: De gesorteerde lijst
+    """
     for i in range(1, int(len(str(max(data))))+1):
         buckets = [[], [], [], [], [], [], [], [], [], []]
         for item in data[:]:  # distribution pass
@@ -31,7 +47,16 @@ def bucketSortFunctional(data: list) -> list:
     return data
 
 
-def bucketSort(data):
+def bucketSort(data: list) -> list:
+    """
+    Hij splits de data op in positieve en negatieven getallen. De negatieve getallen worden positief gemaakt, gesorteerd, positief gemaakt en tenslotte wordt de lijst omgedraaid.
+    De positieve getallen worden alleen gesorteerd.
+    Hij sorteerd de lijst met de functionaliteit van het bucketSort algoritme.
+    Hij past de negatieve lijst aan omdat de functionaliteit van het bucketSort algoritme alleen werkt op positieve integers.
+
+    :param data: De lijst die gesorteerd moet worden.
+    :return: De gesorteerde lijst
+    """
     negative = []
     positive = []
     for item in data:
@@ -49,7 +74,17 @@ def bucketSort(data):
     return negative + positive
 
 
-def bucketSortRecursive(data):
+def bucketSortRecursive(data: list) -> list:
+    """
+    Hij splits de data op in positieve en negatieven getallen. De negatieve getallen worden positief gemaakt, gesorteerd, positief gemaakt en tenslotte wordt de lijst omgedraaid.
+    De positieve getallen worden alleen gesorteerd.
+    Hij sorteerd de lijst met de functionaliteit van het bucketSort algoritme.
+    Hij past de negatieve lijst aan omdat de functionaliteit van het bucketSort algoritme alleen werkt op positieve integers.
+    Hij geeft de functionaliteit van het bucketSort algoritme mee hoevaak die zichzelf moet aanroepen, omdat de recursieve variant dit vereist.
+
+    :param data: De lijst die gesorteerd moet worden.
+    :return: De gesorteerde lijst
+    """
     negative = []
     positive = []
     for item in data:
@@ -67,7 +102,17 @@ def bucketSortRecursive(data):
     return negative + positive
 
 
-def bucketSortFloats(data: list, sortAlgoritme = bucketSort):
+def bucketSortFloats(data: list, sortAlgoritme = bucketSort) -> list:
+    """
+    Deze functie maakt het mogelijk om met de functionaliteit van het bucketSort algoritme te kunnen werken met floats.
+    Hij vermenigvuldigd elk item in de lijst met oneindig (max int van python) en cast dit naar een int om de ".0" weg te krijgen.
+    Nu kan de lijst gesorteerd worden met als standaard het bucketSort algoritme.
+    Dan worden alle items weer gedeeld door oneindig (max int van python) en het zijn weer de orginele items.
+
+    :param data: De lijst met floats die gesorteerd moet worden.
+    :param sortAlgoritme: Het sorteer algoritme, standaard bucketSort.
+    :return: De gesorteerde lijst.
+    """
     multiply = sys.maxsize
     data = [int(item * multiply) for item in data]
     data = sortAlgoritme(data)
